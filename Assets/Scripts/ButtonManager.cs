5e994d9472c2495eb2637762fa693bcf332c4a1c
TWITexe <kawaii_senpaii@mail.ru>
2024-03-07 02:31:54 +0700

    Code Review

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -9,27 +9,48 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] private GameObject mainScreen;
     [SerializeField] private GameObject secondScreen;
     [SerializeField] private GameObject censorship;
+    [SerializeField] private GameObject frontTriangle;
+    [SerializeField] private GameObject letterPath;
+    [SerializeField] private GameObject memory;
 
     public void StartButton()
     {
-        homeButton.SetActive(true);
-        mainScreen.SetActive(false);
-        censorship.SetActive(false);
-        secondScreen.SetActive(true);
+        SetActiveObjects(true, false, true, false, false, false, false);
     }
+
+    public void Censorship()
+    {
+        SetActiveObjects(true, false, false, true, false, false, false);
+    }
+
     public void FrontTriangle()
     {
-        homeButton.SetActive(true);
-        mainScreen.SetActive(false);
-        censorship.SetActive(true);
-        secondScreen.SetActive(false);
+        SetActiveObjects(true, false, false, false, true, false, false);
+    }
+
+    public void Memory()
+    {
+        SetActiveObjects(true, false, false, false, false, false, true);
+    }
+
+    public void LetterPath()
+    {
+        SetActiveObjects(true, false, false, false, false, true, false);
     }
 
     public void Home()
     {
-        homeButton.SetActive(false);
-        mainScreen.SetActive(true);
-        censorship.SetActive(false);
-        secondScreen.SetActive(false);
+        SetActiveObjects(false, true, false, false, false, false, false);
     }
+    private void SetActiveObjects(bool homeButtonActive, bool mainScreenActive, bool secondScreenActive, bool censorshipActive, bool frontTriangleActive, bool letterPathActive, bool memoryActive)
+    {
+        homeButton.SetActive(homeButtonActive);
+        mainScreen.SetActive(mainScreenActive);
+        secondScreen.SetActive(secondScreenActive);
+        censorship.SetActive(censorshipActive);
+        frontTriangle.SetActive(frontTriangleActive);
+        letterPath.SetActive(letterPathActive);
+        memory.SetActive(memoryActive);
+    }
+    
 }
