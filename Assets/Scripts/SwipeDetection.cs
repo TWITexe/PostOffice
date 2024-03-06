f0bfcfd37e21c43662a3b07b1abc23c8e3c9d193
TWITexe <kawaii_senpaii@mail.ru>
2024-03-07 03:14:45 +0700

    Mini CodeReview

--- Assets/Scripts/SwipeDetection.cs
+++ Assets/Scripts/SwipeDetection.cs
@@ -2,66 +2,61 @@ using UnityEngine;
 
 public class SwipeDetection : MonoBehaviour
 {
-    private Vector2 fingerDownPosition;
+    private Vector2 fingerDownPosition; 
     private Vector2 fingerUpPosition;
+    
+    // Определяет, должен ли происходить обнаружение свайпа только после отпускания пальца
+    [SerializeField] private bool detectSwipeOnlyAfterRelease = false; 
+    [SerializeField] private float minDistanceForSwipe = 20f; 
 
-    [SerializeField]
-    private bool detectSwipeOnlyAfterRelease = false;
-
-    [SerializeField]
-    private float minDistanceForSwipe = 20f;
-
+    // Обновил позицию отпускания
     public delegate void SwipeAction(SwipeDirection direction);
     public static event SwipeAction OnSwipe;
 
     public enum SwipeDirection
     {
-        Left,
+        Left, 
         Right
     }
 
     private void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0) 
         {
-            Touch touch = Input.GetTouch(0);
+            Touch touch = Input.GetTouch(0); 
 
-            if (touch.phase == TouchPhase.Began)
+            switch (touch.phase)
             {
-                fingerDownPosition = touch.position;
-                fingerUpPosition = touch.position;
-            }
-
-            if (!detectSwipeOnlyAfterRelease && touch.phase == TouchPhase.Moved)
-            {
-                fingerUpPosition = touch.position;
-                DetectSwipe();
-            }
-
-            if (touch.phase == TouchPhase.Ended)
-            {
-                fingerUpPosition = touch.position;
-                DetectSwipe();
+                // палец только коснулся экрана
+                case TouchPhase.Began: 
+                    fingerDownPosition = touch.position; // Запоминаем позицию касания
+                    fingerUpPosition = touch.position; // Инициализируем позицию отпускания также
+                    break;
+                case TouchPhase.Moved:
+                    if (!detectSwipeOnlyAfterRelease)
+                    {
+                        // Обновляем позицию отпускания
+                        fingerUpPosition = touch.position; 
+                        DetectSwipe();
+                    }
+                    break;
+                // палец был отпущен
+                case TouchPhase.Ended: 
+                    fingerUpPosition = touch.position; 
+                    break;
             }
         }
     }
-
+    
     private void DetectSwipe()
     {
-        if (Vector2.Distance(fingerDownPosition, fingerUpPosition) >= minDistanceForSwipe)
+        if (Vector2.Distance(fingerDownPosition, fingerUpPosition) >= minDistanceForSwipe) 
         {
+            // Вычисляем вектор направления свайпа
             Vector2 direction = fingerUpPosition - fingerDownPosition;
-
-            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) 
             {
-                if (direction.x > 0)
-                {
-                    OnSwipe?.Invoke(SwipeDirection.Right);
-                }
-                else
-                {
-                    OnSwipe?.Invoke(SwipeDirection.Left);
-                }
+                OnSwipe?.Invoke(direction.x > 0 ? SwipeDirection.Right : SwipeDirection.Left);
             }
         }
     }
